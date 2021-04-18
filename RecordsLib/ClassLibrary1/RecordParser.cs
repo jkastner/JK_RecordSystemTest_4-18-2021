79644f21043ed6482f38710d065293bf69fc031a
Johnathan Kastner <johnathan.kastner@gmail.com>
2021-04-18 16:38:52 -0400

    Added dummied data

---
+++ RecordsLib/ClassLibrary1/RecordParser.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RecordsLib
+{
+    public class RecordParser
+    {
+        public IEnumerable<Record> FromString(string [] recordLines)
+        {
+            
+
+        }
+    }
+}
