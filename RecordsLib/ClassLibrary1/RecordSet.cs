f65c12fb44da1a1be835610b31bda1344e688f1e
Johnathan Kastner <johnathan.kastner@gmail.com>
2021-04-18 17:43:02 -0400

    Sorting via linq

--- RecordsLib/ClassLibrary1/RecordSet.cs
+++ RecordsLib/ClassLibrary1/RecordSet.cs
@@ -1,22 +1,28 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace RecordsLib
 {
     public class RecordSet
     {
-        //public IEnumerable<Record> ByGender()
-        //{
-
-        //}       
-        //public IEnumerable<Record> ByBirthDate()
-        //{
-
-        //}    
-        //public IEnumerable<Record> ByLastName()
-        //{
-
-        //}
+        private List<Record> data = new List<Record>();
+        
+        public IEnumerable<Record> ByGender()
+        {
+            return data.OrderBy(x => x.Gender).
+                ThenBy(x=>x.LastName);
+        }      
+        
+        public IEnumerable<Record> ByBirthDate()
+        {
+            return data.OrderBy(x => x.DateOfBirth);
+        }
+        
+        public IEnumerable<Record> ByLastName()
+        {
+            return data.OrderByDescending(x => x.LastName);
+        }
     }
 }
